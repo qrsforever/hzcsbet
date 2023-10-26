21f2ac181d25acbe728bebdbfec80749fe5d01fe
Li Dong <erlangai@qq.com>
2023-10-26 17:19:20 +0800

    save

--- gamebet/scripts/train_pix2pix.sh
+++ gamebet/scripts/train_pix2pix.sh
@@ -3,8 +3,18 @@
 CUR_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 TOP_DIR=$(cd ${CUR_DIR}/..; pwd)
 
+PHASE=${1:-seg}
+GPUID=-1
+
+if [[ x$PHASE == xseg ]]
+then
+    PHASE_DATASET=train_phase_1
+else
+    PHASE_DATASET=train_phase_2
+fi
+
 python3 ${TOP_DIR}/train_pix2pix.py --dataroot ${TOP_DIR}/datasets/soccer_seg_detection \
-    --gpu_ids -1 --phase train_phase_1 --display_id -1 --print_freq 10 \
-    --name soccer_seg_detection_pix2pix --model pix2pix --netG unet_256 \
-    --direction AtoB --dataset_mode aligned \
+    --gpu_ids ${GPUID} --phase ${PHASE_DATASET} --print_freq 10 \
+    --name soccer_${PHASE}_pix2pix --model pix2pix --netG unet_256 \
+    --direction AtoB --dataset_mode aligned --output_nc 1 \
     --gan_mode vanilla --norm batch --pool_size 0
