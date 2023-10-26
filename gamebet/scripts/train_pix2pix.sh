da0928cf08689254d6fd7037ae508e313524848e
Li Dong <erlangai@qq.com>
2023-10-26 17:21:31 +0800

    save

--- gamebet/scripts/train_pix2pix.sh
+++ gamebet/scripts/train_pix2pix.sh
@@ -14,7 +14,7 @@ else
 fi
 
 python3 ${TOP_DIR}/train_pix2pix.py --dataroot ${TOP_DIR}/datasets/soccer_seg_detection \
-    --gpu_ids ${GPUID} --phase ${PHASE_DATASET} --print_freq 10 \
+    --gpu_ids ${GPUID} --phase ${PHASE_DATASET} --display_id -1 --print_freq 10 \
     --name soccer_${PHASE}_pix2pix --model pix2pix --netG unet_256 \
     --direction AtoB --dataset_mode aligned --output_nc 1 \
     --gan_mode vanilla --norm batch --pool_size 0
