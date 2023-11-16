02a0cb1fd9606a1fc3202d698f9d7c81182e0d5f
Li Dong <erlangai@qq.com>
2023-11-16 23:04:09 +0800

    modify

--- gamebet/scripts/train_siamese.sh
+++ gamebet/scripts/train_siamese.sh
@@ -3,7 +3,7 @@
 CUR_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 TOP_DIR=$(cd ${CUR_DIR}/..; pwd)
 
-mkdir ${TOP_DIR}/checkpoints
+[ ! -d mkdir ${TOP_DIR}/checkpoints ] && mkdir ${TOP_DIR}/checkpoints
 
 python3 ${TOP_DIR}/train_siamese.py --data-path ${TOP_DIR}/data/dataset_sample.mat \
     --lr 0.1 --num-epoch 300 --batch-size 512 --resume-from ${TOP_DIR}/checkpoints/siamese.pth
