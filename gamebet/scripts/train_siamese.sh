aaf4e53e1f391ca9a66f37555d0a83acb0cd959c
Li Dong <erlangai@qq.com>
2023-11-17 18:46:11 +0800

    save

--- gamebet/scripts/train_siamese.sh
+++ gamebet/scripts/train_siamese.sh
@@ -3,7 +3,8 @@
 CUR_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
 TOP_DIR=$(cd ${CUR_DIR}/..; pwd)
 
-[ ! -d mkdir ${TOP_DIR}/checkpoints ] && mkdir ${TOP_DIR}/checkpoints
+[ ! -d ${TOP_DIR}/checkpoints ] && mkdir ${TOP_DIR}/checkpoints
 
 python3 ${TOP_DIR}/train_siamese.py --data-path ${TOP_DIR}/data/dataset_sample.mat \
-    --lr 0.1 --num-epoch 300 --batch-size 512 --resume-from ${TOP_DIR}/checkpoints/siamese.pth
+    --lr 0.1 --num-epoch 100 --batch-size 512 --num-workers $(nproc) \
+    --resume-from ${TOP_DIR}/checkpoints/siamese.pth
--- gamebet/test_siamese.py
+++ gamebet/test_siamese.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# @file train_siamese.py
+# @file test_siamese.py
 # @brief
 # @author QRS
 # @version 1.0
-# @date 2023-11-14 21:44
+# @date 2023-11-17 14:54
 
 
 import os
@@ -26,12 +26,9 @@ from models.siamese import ContrastiveLoss, SiameseNetwork
 parser = argparse.ArgumentParser()
 parser.add_argument('--data-path', required=True, type=str, help='a .mat file')
 parser.add_argument('--cuda-id', required=False, type=int, default=0, help='CUDA ID 0, 1, 2, 3')
-parser.add_argument('--lr', required=True, type=float, default=0.01, help='learning rate')
 parser.add_argument('--batch-size', required=True, type=int)
-parser.add_argument('--num-epoch', required=True, type=int, help='epoch number')
 parser.add_argument('--num-workers', required=False, type=int, default=4, help='job worker number')
-
-parser.add_argument('--resume-from', default='', type=str, help='path to the save checkpoint')
+parser.add_argument('--weights-path', default='', type=str, help='path of checkpoint weights')
 
 args = parser.parse_args()
 
@@ -40,20 +37,17 @@ if not os.path.exists(args.data_path):
 
 dataset_sample = sio.loadmat(args.data_path)
 
-model_save_path = args.resume_from
-
-epoch_beg = 0
-epoch_num = args.num_epoch
-
 device = torch.device('cpu')
 if torch.cuda.is_available():
     device = torch.device('cuda:{}'.format(args.cuda_id))
     cudnn.benchmark = True
 
-model_state_dict, optim_state_dict = None, None
-if os.path.exists(args.resume_from):
+model_state_dict = None
+if os.path.exists(args.weights_path):
     ckpts = torch.load(args.resume_from, map_location=device)
-    model_state_dict, optim_state_dict, epoch_beg = ckpts['model'], ckpts['optimizer'], ckpts['epoch']
+    model_state_dict = ckpts['model']
+else:
+    raise
 
 ## Dataset
 
@@ -65,7 +59,7 @@ data_transform = transforms.Compose([
 
 data = CameraPair(anchor_images=dataset_sample['pivot_images'], positive_images=dataset_sample['positive_images'])
 dataset = CameraPairDataset(data, transform=data_transform)
-train_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+test_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
 ## Loss
 
@@ -78,50 +72,19 @@ siamese = SiameseNetwork().to(device)
 if model_state_dict is not None:
     siamese.load_state_dict(model_state_dict)
 
-# optimizer = optim.SGD(siamese.parameters(), lr=0.1, momentum=0.9)
-optimizer = optim.Adam(siamese.parameters(), lr=learning_rate)
-if optim_state_dict is not None:
-    optimizer.load_state_dict(optim_state_dict)
-
-# scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 25, 40], gamma=0.1)
-
 ## Train
 
-for epoch in range(epoch_beg, epoch_num + 1):
-    siamese.train()
+with torch.no_grad():
     losses, pos_dists, neg_dists = [], [], []
-    for bx1, bx2, labels in train_loader:
+    for bx1, bx2, labels in test_loader:
         bx1, bx2, labels = bx1.to(device), bx2.to(device), labels.to(device)
         feat1, feat2 = siamese(bx1, bx2)
-        loss = criterion(feat1, feat2, labels)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
         dist = F.pairwise_distance(feat1.detach(), feat2.detach(), keepdim=True)
         for l, d in zip(labels.detach().squeeze(), dist.squeeze()):
             pos_dists.append(d) if l == 1 else neg_dists.append(d)
 
-    scheduler.step()
-
     dist_pos, dist_neg = torch.mean(torch.tensor(pos_dists)), torch.mean(torch.tensor(neg_dists))
     print('[%d] lr=[%.6f] loss[%.6f] pos_d[%.6f] neg_d[%.6f] ratio[%.6f]' % (
-        epoch,
-        optimizer.param_groups[0]['lr'],
         torch.mean(torch.tensor(losses)), dist_pos, dist_neg,
         dist_neg / (dist_pos + 0.000001) 
         ))
-
-    if (epoch + 1) % 50 == 0:
-        torch.save({
-            'epoch': epoch + 1,
-            'model': siamese.state_dict(),
-            'optimizer': optimizer.state_dict()
-        }, model_save_path)
-
-torch.save({
-    'epoch': epoch + 1,
-    'model': siamese.state_dict(),
-    'optimizer': optimizer.state_dict()
-}, model_save_path)
