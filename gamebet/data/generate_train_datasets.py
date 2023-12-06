e1c1272e6586f0cdc192ed8e9320b011cdf984d9
Li Dong <erlangai@qq.com>
2023-12-06 22:47:23 +0800

    save

--- gamebet/data/generate_train_datasets.py
+++ gamebet/data/generate_train_datasets.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import os
+TOP_DIR='/data/k12ai/codes/hzcsbet'
+os.environ['PYTHONPATH'] = TOP_DIR
+
 import scipy.io as sio
 from util.synthetic import generate_siamese_dataset
 
-TOP_DIR='/data/k12ai/codes/hzcsbet'
 N = 9999
-camera_parameter_file = f'{TOP_DIR}/data/worldcup_dataset_camera_parameter.mat'
-soccer_field_template_file = f'{TOP_DIR}/data/worldcup2014.mat'
-dataset_sample_file = f'{TOP_DIR}/data/dataset_sample_{N}.mat'
+
+camera_parameter_file = f'{TOP_DIR}/gamebet/data/worldcup_dataset_camera_parameter.mat'
+soccer_field_template_file = f'{TOP_DIR}/gamebet/data/worldcup2014.mat'
+dataset_sample_file = f'{TOP_DIR}/gamebet/data/dataset_sample_{N}.mat'
 
 generate_siamese_dataset(camera_parameter_file, soccer_field_template_file, dataset_sample_file, image_num=N)
 dataset_sample = sio.loadmat(dataset_sample_file)
--- scripts/start_cv_docker.sh
+++ scripts/start_cv_docker.sh
@@ -1,3 +1,4 @@
 docker run -itd --name gamebet --entrypoint bash --network host \
     --shm-size 10g --memory 10g --volume /data/k12ai:/data/k12ai \
-    --runtime nvidia hzcsai_com/k12ai
+    --workdir /data/k12ai/codes/hzcsbet
+    --runtime nvidia hzcsai_com/k12cv
